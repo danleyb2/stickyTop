102925687367d4a44ddd82110d3400478427aad2
danleyb2 <ndieksman@gmail.com>
2015-11-08 22:27:38 +0300

    create onmouse press and drag action handlers to move note onscreen

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,15 +1,20 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ToolBar;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
+
+
 public class Controller {
+    public WindowLocationPoint wlp=new WindowLocationPoint();
     public void newNote(ActionEvent actionEvent) {
         //new Note(Main.STAGE);
         new Note();
-
     }
 
     @FXML private Button btnClose;
@@ -18,7 +23,24 @@ public class Controller {
         Stage st=(Stage)btnClose.getScene().getWindow();
         st.close();
 
+    }
+    @FXML public Stage window;
+    @FXML private ToolBar toolBar;
+    public void toolbarMousePressed(Event event) {
 
+        window=(Stage)toolBar.getScene().getWindow();
+        MouseEvent mouseEvent=((MouseEvent) event);
+
+        wlp.setX(window.getX()- mouseEvent.getScreenX());
+        wlp.setY(window.getY() - mouseEvent.getScreenY());
+    }
+
+    @FXML
+    public void toolbarMouseDragged(Event event) {
+        MouseEvent mouseEvent=((MouseEvent) event);
+
+            window.setX(mouseEvent.getScreenX()+wlp.getX());
+        window.setY(mouseEvent.getScreenY()+wlp.getY());
 
     }
 }
