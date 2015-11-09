3bea9f9c8115126698fcbf247a8dda1a517150a7
danleyb2 <ndieksman@gmail.com>
2015-11-09 19:14:17 +0300

    create a new note beside trigger note.
    default position is left unless no enough width (300)

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -7,14 +7,19 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ToolBar;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
-
+import javafx.stage.Window;
 
 
 public class Controller {
     public WindowLocationPoint wlp=new WindowLocationPoint();
+    @FXML public Stage window;
+    @FXML private ToolBar toolBar;
+
     public void newNote(ActionEvent actionEvent) {
-        //new Note(Main.STAGE);
-        new Note();
+        Window window=toolBar.getScene().getWindow();
+        double x=window.getX();
+        double w=window.getWidth();
+        new Note((x>w)? x-w: x+w,window.getY());
     }
 
     @FXML private Button btnClose;
@@ -24,8 +29,8 @@ public class Controller {
         st.close();
 
     }
-    @FXML public Stage window;
-    @FXML private ToolBar toolBar;
+
+
     public void toolbarMousePressed(Event event) {
 
         window=(Stage)toolBar.getScene().getWindow();
@@ -39,7 +44,7 @@ public class Controller {
     public void toolbarMouseDragged(Event event) {
         MouseEvent mouseEvent=((MouseEvent) event);
 
-            window.setX(mouseEvent.getScreenX()+wlp.getX());
+        window.setX(mouseEvent.getScreenX()+wlp.getX());
         window.setY(mouseEvent.getScreenY()+wlp.getY());
 
     }
